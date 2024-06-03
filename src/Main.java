6ca4d39c2eab7510c7a58e4ccd28f346d6ce6479
ph.zh <p.zhavoronkov@gmail.com>
2024-06-03 23:57:17 +0200

    loop task 1 upload

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+Байт тем временем налёг на новый корм — съедает по целой пачке в день.
+Ветеринар считает, что это не очень полезно, и порекомендовал делить пачку на три дня.
+Вы, как обычно, отложили на корм 500 рублей. Подсчитайте с помощью цикла, на сколько дней вам хватит
+этих денег, если одна пачка корма для Байта стоит 100 рублей. Ваша программа должна выводить каждый
+трёхдневный период и остаток средств к этому моменту:
+На 3-й день останется 400 рублей.
+На 6-й день останется 300 рублей.
+На 9-й день останется 200 рублей.
+На 12-й день останется 100 рублей.
+На 15-й день останется 0 рублей.
+Денег хватит на 15 дней.
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int money = 500; // Деньги на корм
+        int foodPerDay = 100; // Стоимость пачки корма
+        int days = 0; // Дни, когда вы сможете покупать корм
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Здесь опишите цикл, начинаем с 500 рублей
+        // Здесь отразите, что вы будете покупать корм каждые три дня
+        // Опишите строку вывода
+        // Цикл оканчивается здесь
+        for (int i = money; i > 0; i = i - foodPerDay) {
+            for (int j = days; j <= 15; j = j ++) {
+                System.out.println("На " + j + "-й день останется " + (i - foodPerDay) + " рублей.");
+            }
         }
+        System.out.println("Денег хватит на " + days + " дней.");
     }
 }
