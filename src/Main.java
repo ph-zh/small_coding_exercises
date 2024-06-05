677176ba5bcefb5b54b423cf70224829b309e82b
ph.zh <p.zhavoronkov@gmail.com>
2024-06-05 20:11:39 +0200

    loop task with floor and apartment number with updated logic

--- src/Main.java
+++ src/Main.java
@@ -17,13 +17,14 @@
  */
 public class Main {
     public static void main(String[] args) {
+
         int flatsNumber = 5; // Количество квартир на этаже
         int floorsNumber = 10; // Количество этажей
 
-        for (int i = 1; i <= flatsNumber; i++) {
-            for (int j = 1; j <= floorsNumber; j++) {
-                int currentFlat = (j - 1) * i + flatsNumber; // Заготовка формулы для учёта квартир
-                System.out.println("Этаж " + j + " квартира " + currentFlat + " — доставлено.");
+        for (int i = 1; i <= floorsNumber; i++) {
+            for (int j = 1; j <= flatsNumber; j++) {
+                int currentFlat = (i - 1) * flatsNumber + j; // Заготовка формулы для учёта квартир
+                System.out.println("Этаж " + i + " квартира " + currentFlat + " — доставлено.");
             }
         }
     }
