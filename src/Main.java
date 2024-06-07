c83de2c29bccdb6c6c09d7f03a1597f26e562e07
ph.zh <p.zhavoronkov@gmail.com>
2024-06-07 13:30:03 +0200

    loop while with bank saving

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 /*
-
+Накопления — дело серьёзное. Ваш друг уже накопил 5000 рублей и решил замахнуться на миллион. Для этого он положил свои сбережения
+на накопительный счёт под 5% годовых. Допишите программу с циклом  while, которая посчитает, через сколько лет на счету будет
+нужная сумма. Предполагается, что стартовый баланс счёта (сумма сбережений) вводится с консоли.
  */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int foodWeight = 500; // Количество корма
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько денег у вас сейчас: ");
+        double balance = scanner.nextDouble(); // Ваши сбережения
+        int years = 0;
+        int goal = 1000000;
 
-        while (foodWeight >= 10) { // Условие работы цикла - пока осталась хотя бы одна порция
-            foodWeight = foodWeight - 10; // Байт съедает 10 грамм корма за раз
+        while (balance <= goal) {
+            balance = balance + balance * 0.05;
+            years = years + 1;
         }
 
-        System.out.println("Корм закончился! Пора идти в магазин!");
+        System.out.println("Через " + years + " лет у вас будет миллион!");
     }
 }
