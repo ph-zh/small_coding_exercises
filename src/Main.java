3731e87195cc9f7f87183b143191778e3a92e69d
ph.zh <p.zhavoronkov@gmail.com>
2024-06-08 23:35:46 +0200

    array with expenses initialization

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
 /*
-Расходы за неделю записаны в семи отдельных переменных. Соберите все их значения в один массив с
-именем expenses (англ. «расходы»). Не забудьте, что тип данных массива должен соответствовать его содержанию.
+Перед вами массив трат за неделю. Каждый его элемент — это сумма, потраченная за один день, от понедельника до воскресенья. Сделайте следующее:
+Исправьте сумму расходов за среду: в ней не отражено мороженое, купленное за 115 рублей. Прибавьте стоимость десерта к расходам за день.
+Суммируйте три самые крупные траты в массиве — это расходы за вторник, пятницу и субботу. Результат сохраните в переменную sum.
  */
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Расходы за неделю
-        double mondayExpense = 100.50;
-        double tuesdayExpense = 500.00;
-        double wednesdayExpense = 10.00;
-        double thursdayExpense = 0.0;
-        double fridayExpense = 40.60;
-        double saturdayExpense = 500.10;
-        double sundayExpense = 200.00;
+        double[] expenses = {100.50, 500.00, 10.00, 0.0, 640.60, 370.20, 200.00};
 
-        // Объявите массив expenses и соберите в него значения из переменных
-        double[] expenses = {mondayExpense, tuesdayExpense, wednesdayExpense, thursdayExpense, fridayExpense, saturdayExpense, sundayExpense};
+        // Добавьте 115 рублей к расходам за среду
+        expenses[2] = expenses[2] + 115;
+        System.out.println("Новое значение расходов за среду: " + expenses[2] + " рублей.");
+
+        // Суммируйте три самые крупные траты
+        double sum = expenses[1] + expenses[4] + expenses[5];
+        System.out.println("Самые большие расходы были во вторник, пятницу и субботу.");
+        System.out.println("Всего вы потратили в эти дни: " + sum + " рублей.");
     }
 }
