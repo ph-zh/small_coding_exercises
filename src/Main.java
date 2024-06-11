9fb1c9b90d78f0807c421b57da0824d8b39ec037
ph.zh <p.zhavoronkov@gmail.com>
2024-06-11 23:40:44 +0200

    methods task to compare max value movie updated logic for max value through for loop

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@
 public class Main {
     public static void main(String[] args) {
         // Ниже вызовите новый метод
-        String highestGrossingFilm = ...;
+        String highestGrossingFilm = findHighestGrossingFilm();
         System.out.println("Самый кассовый фильм: " + highestGrossingFilm);
     }
 
@@ -23,6 +23,25 @@ public class Main {
         int income3 = 1084;
 
         // Допишите реализацию метода ниже
-        ...
+        if (income1 > income2) {
+            if (income1 > income3) {
+                return film1;
+            } else {
+                return film3;
+            }
+        } else if (income2 > income1) {
+            if (income2 > income3) {
+                return film2;
+            } else {
+                return film3;
+            }
+        } else if (income3 > income1) {
+            if (income3 > income2) {
+                return film3;
+            } else {
+                return film2;
+            }
+        }
+        return film1;
     }
 }
