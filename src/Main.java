9a310e9b04063cf721233547b84dc39b3615be23
ph.zh <p.zhavoronkov@gmail.com>
2024-06-13 16:32:08 +0200

    method with find max value task

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
 /*
+Метод findMax() сейчас может работать только с фиксированными значениями — 3 и 5.
+Исправьте код метода таким образом, чтобы он мог принимать два любых целых значения.
+Затем по аналогии вызовите метод, чтобы напечатать результат сравнения ещё двух пар чисел: 16 и 5, -1 и -7.
  */
 
 public class Main {
+
     public static void main(String[] args) {
-        printUserInfo("Катя", 26, "Москва");
-        printUserInfo("Серёжа", 40, "Воронеж");
+        System.out.println("Наибольшее из чисел 3 и 5 = " + findMax(3, 5));
+        // Напишите аналогичный вызов метода findMax для таких пар чисел: 16 и 5, -1 и -7
+        System.out.println("Наибольшее из чисел 16 и 5 = " + findMax(16, 5));
+        System.out.println("Наибольшее из чисел -1 и -7 = " + findMax(-1, -7));
     }
 
-    public static void printUserInfo(String username, int age, String city) {
-        System.out.println("Меня зовут " + username + ", я из города " + city + ", мне " + age + " лет.");
+    public static int findMax(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
     }
-
 }
