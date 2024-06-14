46cca6565a058a092af14a2f6d938faf336f44ed
ph.zh <p.zhavoronkov@gmail.com>
2024-06-14 11:18:48 +0200

    methods created. Code decomposed. Logic of return statements are missing

--- src/Main.java
+++ src/Main.java
@@ -19,40 +19,13 @@ public class Main {
         double[] feedExpensesCat = {100.50, 236.0, 510.6, 150.20, 80.0, 172.0, 135.4};
         double[] feedExpensesHamster = {70.50, 146.0, 710.6, 250.20, 83.0, 19.0, 55.4};
 
+        findExpensesSum(feedExpensesCat, feedExpensesHamster);
+        System.out.println("Всего на корм было потрачено " + feedExpensesCat);
+        System.out.println("Всего на корм было потрачено " + feedExpensesHamster);
 
-        double maxFeedExpenseCat = 0;
-        for (int i = 0; i < feedExpensesCat.length; i++) {
-            if (feedExpensesCat[i] > maxFeedExpenseCat) {
-                maxFeedExpenseCat = feedExpensesCat[i];
-            }
-        }
-
-        System.out.println("Твой самый дорогой корм стоил " + maxFeedExpenseCat);
-
-        double maxFeedExpenseHamster = 0;
-        for (int i = 0; i < feedExpensesHamster.length; i++) {
-            if (feedExpensesHamster[i] > maxFeedExpenseHamster) {
-                maxFeedExpenseHamster = feedExpensesHamster[i];
-            }
-        }
-
-        System.out.println("Твой самый дорогой корм стоил " + maxFeedExpenseHamster);
-
-        double sumFeedCat = 0;
-        for (int i = 0; i < feedExpensesCat.length; i++) {
-            sumFeedCat = sumFeedCat + feedExpensesCat[i];
-        }
-
-        System.out.println("Всего на корм было потрачено " + sumFeedCat);
-
-        double sumFeedHamster = 0;
-        for (int i = 0; i < feedExpensesHamster.length; i++) {
-            sumFeedHamster = sumFeedHamster + feedExpensesHamster[i];
-        }
-
-        System.out.println("Всего на корм было потрачено " + sumFeedHamster);
-
-
+        findMaxExpense(feedExpensesCat, feedExpensesHamster);
+        System.out.println("Твой самый дорогой корм стоил " + feedExpensesCat);
+        System.out.println("Твой самый дорогой корм стоил " + feedExpensesHamster);
     }
 
     public static void sayHello(String catName, String hamsterName) {
@@ -65,7 +38,33 @@ public class Main {
         System.out.println("Приятного аппетита, " + hamsterName + "!");
     }
 
-    public static void findMaxExpense() {
-        
+    public static double findMaxExpense(double[] feedExpensesCat, double[] feedExpensesHamster) {
+        double maxFeedExpenseCat = 0;
+        for (int i = 0; i < feedExpensesCat.length; i++) {
+            if (feedExpensesCat[i] > maxFeedExpenseCat) {
+                maxFeedExpenseCat = feedExpensesCat[i];
+            }
+        }
+
+        double maxFeedExpenseHamster = 0;
+        for (int i = 0; i < feedExpensesHamster.length; i++) {
+            if (feedExpensesHamster[i] > maxFeedExpenseHamster) {
+                maxFeedExpenseHamster = feedExpensesHamster[i];
+            }
+        }
+        return maxFeedExpenseCat;
+    }
+
+    public static double findExpensesSum(double[] feedExpensesCat, double[] feedExpensesHamster) {
+        double sumFeedCat = 0;
+        for (int i = 0; i < feedExpensesCat.length; i++) {
+            sumFeedCat = sumFeedCat + feedExpensesCat[i];
+        }
+
+        double sumFeedHamster = 0;
+        for (int i = 0; i < feedExpensesHamster.length; i++) {
+            sumFeedHamster = sumFeedHamster + feedExpensesHamster[i];
+        }
+        return sumFeedCat;
     }
 }
