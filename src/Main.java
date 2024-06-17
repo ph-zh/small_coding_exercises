de1fb3a64bea73ed9dd1c1107df0249292cb0e8b
ph.zh <p.zhavoronkov@gmail.com>
2024-06-17 14:49:08 +0200

    method eat added with argument 15

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 /*
-Переменная называется homka, но хомяка все еще зовут Байт. Измените имя хомяка на Хомку, а заодно отредактируйте
-и другие атрибуты: пусть ему будет 1 год, он будет весить 420 грамм, а его цвет будет не рыжий, а чёрный.
+Хомка голодный! Покормите его с помощью метода eat. В качестве аргумента передайте число 15.
+После этого проверьте значение его веса снова, чтобы убедиться, что он поужинал.
  */
 
 public class Main {
     public static void main(String[] args) {
         Hamster homka = new Hamster();
-
+        // Присвойте полям новые значения
         homka.name = "Хомка";
         homka.age = 1;
         homka.weight = 420;
-        homka.color = "чёрный";
+        homka.color = "Чёрный";
 
         System.out.println("Имя: " + homka.name);
         System.out.println("Возраст: " + homka.age);
-        System.out.println("Вес: " + homka.weight);
-        System.out.println("Цвет: " + homka.color);
+        System.out.println("Вес: " +  homka.weight);
+        System.out.println("Цвет: " +  homka.color);
+
+        // Покормите хомяка и напечатайте его вес
+        homka.eat(15);
+        System.out.println("Новый вес: " + homka.weight);
     }
 }
