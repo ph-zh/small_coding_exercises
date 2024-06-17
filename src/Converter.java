51b127749a1b02101b819f02754414a52b8993af
ph.zh <p.zhavoronkov@gmail.com>
2024-06-17 14:10:37 +0200

    method convert added with attributes (fields)

--- src/Converter.java
+++ src/Converter.java
@@ -1,12 +1,24 @@
 /*
-Теперь добавьте классу  Converter  поля и присвойте им значения: курс доллара, евро и японской иены.
-Имена полей будут совпадать с теми, что вы использовали ранее:  rateUSD ,  rateEUR ,  rateJPY .
+Вам нужно, чтобы конвертер умел рассчитать соотношение валют.
+Для этого объявите метод: convert. Этот метод должен принимать такие параметры, как сумма в рублях (double rubles)
+и код валюты (int currency). Напишите определение метода и поместите в него логику конвертации валюты.
  */
+
 public class Converter {
-    //доллар = 78.5;
-    //евро = 88.7;
-    //иена = 0.75;
-    double rateUSD = 78.8;
-    double rateEUR = 88.7;
-    double rateJPY = 0.75;
+    // Тело метода
+    public static void convert(double rubles, int currency) {
+        double rateUSD = 78.5;
+        double rateEUR = 88.7;
+        double rateJPY = 0.75;
+
+        if (currency == 1) {
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        } else if (currency == 2) {
+            System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
+        } else if (currency == 3) {
+            System.out.println("Ваши сбережения в иенах: " + rubles / rateJPY);
+        } else {
+            System.out.println("Неизвестная валюта");
+        }
+    }
 }
