8e26e48aea5c4498199032c9250a775c427ba012
ph.zh <p.zhavoronkov@gmail.com>
2024-06-17 14:40:44 +0200

    attributes to class Hamster were added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 /*
-Программа не работает — мы пытаемся обратиться к объекту, но он ещё не создан. Исправьте это.
+Переменная называется homka, но хомяка все еще зовут Байт. Измените имя хомяка на Хомку, а заодно отредактируйте
+и другие атрибуты: пусть ему будет 1 год, он будет весить 420 грамм, а его цвет будет не рыжий, а чёрный.
  */
 
 public class Main {
@@ -7,5 +8,8 @@ public class Main {
         Hamster homka = new Hamster();
 
         System.out.println("Имя: " + homka.name);
+        System.out.println("Возраст: " + homka.age);
+        System.out.println("Вес: " + homka.weight);
+        System.out.println("Цвет: " + homka.color);
     }
 }
