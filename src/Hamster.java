2f0c984b151f0061e0ae18a035ac0355607850f7
ph.zh <p.zhavoronkov@gmail.com>
2024-06-19 16:44:51 +0200

    tempalte task to define weight of the class Haster and its instance

--- src/Hamster.java
+++ src/Hamster.java
@@ -1,30 +1,24 @@
 public class Hamster {
-    public static void main(String[] args) {
-        Hamster bite = new Hamster();
-        Hamster ninja = new Hamster();
-        Hamster kuzya = new Hamster();
-        Hamster paskal = new Hamster();
-    }
     //Поля
-        String name = "Байт"; // Имя
-        int age = 2; // Возраст
-        String color = "Рыжий"; // Цвет
-        int weight = 350;
+    String name = "Байт"; // Имя
+    int age = 2; // Возраст
+    String color = "Рыжий"; // Цвет
+    int weight = 350;
 
-        // Методы
-        void eat(int foodWeight) {
-            weight = weight + foodWeight;
-        }
+    // Методы
+    void eat(int foodWeight) {
+        weight = weight + foodWeight;
+    }
 
-        // Бегать в колесе
-        void runInWheel() {
-            System.out.println("Бегу-бегу-бегу!");
-            weight = weight - 5;
-        }
+    // Бегать в колесе
+    void runInWheel() {
+        System.out.println("Бегу-бегу-бегу!");
+        weight = weight - 5;
+    }
 
-        // Прятать семечки
-        void hideSeeds(int seedWeight) {
-            System.out.println("Зимой не заголодаю.");
-            weight = weight + seedWeight;
-        }
+    // Прятать семечки
+    void hideSeeds(int seedWeight) {
+        System.out.println("Зимой не заголодаю.");
+        weight = weight + seedWeight;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
 /*
-
+Прочитайте код и расположите имена хомяков в порядке убывания их веса
  */
-
 public class Main {
+    public static void main (String[] args) {
+        Hamster bite = new Hamster(); // Имя Байта менять не нужно
+        bite.eat(35);
+        for (int i = 0; i < 3; i++) {
+            bite.runInWheel();
+        }
 
+        Hamster ninja = new Hamster();
+        ninja.name = "Ниндзя";
+        ninja.weight = 250;
+        ninja.runInWheel();
+        if (ninja.weight < 265) {
+            ninja.eat(45);
+        }
+
+        Hamster kuzya = new Hamster();
+        kuzya.name = "Кузя";
+        kuzya.weight = 450;
+        kuzya.weight = 325;
+        kuzya.eat(20);
+
+        Hamster paskal = new Hamster();
+        paskal.name = "Паскаль";
+        paskal.weight = 320;
+    }
 }
 
 
