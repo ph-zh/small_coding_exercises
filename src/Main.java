865f8ee9968ce66304c721f97e113c7b72248251
ph.zh <p.zhavoronkov@gmail.com>
2024-06-20 16:55:52 +0200

    template with Classes update

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,24 @@
  */
 public class Main {
     public static void main (String[] args) {
-        // weight 370 - 4
-        Hamster bite = new Hamster(); // Имя Байта менять не нужно
-        bite.eat(35);
-        for (int i = 0; i < 3; i++) {
-            bite.runInWheel();
-            System.out.println(bite.weight);
-        }
-        //weight 290 - 1
+        Hamster bite = new Hamster();
         Hamster ninja = new Hamster();
         ninja.name = "Ниндзя";
+        ninja.age = 1;
         ninja.weight = 250;
-        ninja.runInWheel();
-        if (ninja.weight < 265) {
-            ninja.eat(45);
-            System.out.println(ninja.weight);
-        }
+        ninja.color = "Белый";
 
-        //weight 345 - 3
         Hamster kuzya = new Hamster();
         kuzya.name = "Кузя";
+        kuzya.age = 3;
         kuzya.weight = 450;
-        kuzya.weight = 325;
-        kuzya.eat(20);
-        System.out.println(kuzya.weight);
-        //320 - 2
+        kuzya.color = "Пятнистый";
+
         Hamster paskal = new Hamster();
         paskal.name = "Паскаль";
+        paskal.age = 2;
         paskal.weight = 320;
-        System.out.println(paskal.weight);
+        paskal.color = "Чёрный";
     }
 }
 
