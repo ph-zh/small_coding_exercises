9df6f5f9136f116ca4d7eef908d19f0d75250de3
ph.zh <p.zhavoronkov@gmail.com>
2024-07-08 14:54:56 +0200

    simple task with variables

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,14 @@
 /*
-Как видим, можно вычислить три признака високосного года:
-
-Год является високосным, если год делится на 4 без остатка.
-Год не является високосным, если он полностью делится и на 4, и на 100.
-Год является високосным, если он делится и на 4, и на 100, и на 400.
+Создайте две переменные: целого типа a и действительного b, равные 5 и 0.4 соответственно.
+Выведите построчно на экран их сумму, разность, произведение и частное.
  */
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int year = 0;
-        System.out.println(leapYearCount(year));
-    }
-
-    public static int leapYearCount(int year) {
-        Scanner scanner = new Scanner(System.in);
-        year = scanner.nextInt();
-
-        if (year % 400 == 0) {
-            if (year % 100 == 0) {
-                if (year % 4 == 0) {
-                    System.out.println("Leap Year");
-                    return year;
-                }
-            }
-        } else if (year % 100 == 0) {
-            if (year % 4 == 0) {
-                System.out.println("Not Leap Year");
-                return year;
-            }
-        } else if (year % 4 == 0) {
-            System.out.println("Not Leap Year");
-            return year;
-        }
-        System.out.println("Not Leap Year");
-        return year;
+        int a = 5;
+        double b = 0.4;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
     }
 }
