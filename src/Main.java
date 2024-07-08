826aa38b0dbc132e26a5c5d3402ce3c89942a0fa
ph.zh <p.zhavoronkov@gmail.com>
2024-07-08 13:28:05 +0200

    leap year task

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,35 @@
 Год не является високосным, если он полностью делится и на 4, и на 100.
 Год является високосным, если он делится и на 4, и на 100, и на 400.
  */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int year = 1;
-        }
+        int year = 0;
+        System.out.println(leapYearCount(year));
+    }
 
-        public static int leapYearCount(int year) {
+    public static int leapYearCount(int year) {
+        Scanner scanner = new Scanner(System.in);
+        year = scanner.nextInt();
+
+        if (year % 400 == 0) {
+            if (year % 100 == 0) {
+                if (year % 4 == 0) {
+                    System.out.println("Leap Year");
+                    return year;
+                }
+            }
+        } else if (year % 100 == 0) {
+            if (year % 4 == 0) {
+                System.out.println("Not Leap Year");
+                return year;
+            }
+        } else if (year % 4 == 0) {
+            System.out.println("Not Leap Year");
+            return year;
+        }
+        System.out.println("Not Leap Year");
         return year;
     }
 }
