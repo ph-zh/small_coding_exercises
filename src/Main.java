c6f5646a9edcf1293a8626854de6370a89dd084f
ph.zh <p.zhavoronkov@gmail.com>
2024-07-08 15:32:30 +0200

    same task but with + and - operator

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 /*
-Напишите программу, которая считывает три целых числа и выводит их произведение.
+Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере.
+Пробелы, знаки препинания, заглавные и строчные буквы важны!
+Вводится целое число, по модулю не превосходящее 10000.
+Выведите сначала фразу "The next number for the number ", затем введенное число, затем слово " is ",
+окруженное пробелами, затем формулу для следующего за введенным числа, наконец, знак точки без пробела.
+Аналогично в следующей строке для предыдущего числа.
  */
 import java.util.Scanner;
 
@@ -7,8 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        System.out.println(a * b * c);
+        System.out.println("The next number for the number " + a + " is " + (a + 1) + ".");
+        System.out.println("The previous number for the number " + a + " is " + (a - 1) + ".");
     }
 }
