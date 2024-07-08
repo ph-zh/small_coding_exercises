76ac6fa603a8ddbbea96540f6b867fe8c2028488
ph.zh <p.zhavoronkov@gmail.com>
2024-07-08 16:15:02 +0200

    even numbers task

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 /*
-На одной строчке через пробел записаны два целых числа: длина и ширина прямоугольника.
-Вычислите его площадь и периметр (именно в таком порядке). Результат выведите на разных строках
+Дано целое число n. Выведите следующее за ним четное число.
+При решении этой задачи нельзя использовать условную инструкцию if и циклы.
+Подсказка: Подумайте, что будет оставаться в остатке при делении на 2 у чётных и у нечётных чисел.
  */
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int width = scanner.nextInt();
-        int length = scanner.nextInt();
-        int square = width * length;
-        int perimeter = 2 * (width + length);
-        System.out.println(square);
-        System.out.println(perimeter);
+        int n = scanner.nextInt();
+        int x = 2;
+        x = n - (n % x) + 2;
+        System.out.println(x);
     }
 }
