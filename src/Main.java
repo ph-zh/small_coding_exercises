31b79d37ea616b2ff908905efa65ba59e237dca7
ph.zh <p.zhavoronkov@gmail.com>
2024-07-10 19:01:20 +0200

    type given to variables

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,11 @@
  */
 public class Main {
     public static void main(String[] args) {
-        // Допишите тип для names
-        String[] names = {"Маша", "Саша", "Катя", "Артём"};
-        sayHello(names);
-
-        // Укажите тип переменной
-        int sum = add(3, 6);
-    }
-
-    // Добавьте подходящий тип у параметра метода
-    public static void sayHello(String[] names) {
-        // У переменной итерирования тоже должен быть тип
-        for (int i = 0; i < names.length; i++) {
-            System.out.println("Привет, " + names[i]);
-        }
-    }
-
-    // Вставьте тип возвращаемого значения метода и его параметров
-    public static int add(int a, int b) {
-        return a + b;
+        String hello = "Привет!";
+        byte dayOfMonth = 27;
+        long vvpOfRussia = 4_097_283_854_564L;
+        char signPi = 'π';
+        double numberPi = 3.1415926535897932384626433832795;
+        boolean isResolved = true;
     }
 }
