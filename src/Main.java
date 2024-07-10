854369dd9ca9fa46f8ddcb06bb7f813df54b198e
ph.zh <p.zhavoronkov@gmail.com>
2024-07-10 19:21:15 +0200

    variable initialization

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@
  */
 public class Main {
     public static void main(String[] args) {
-        String hello = "Привет!";
-        byte dayOfMonth = 27;
-        long vvpOfRussia = 4_097_283_854_564L;
-        char signPi = 'π';
-        double numberPi = 3.1415926535897932384626433832795;
-        boolean isResolved = true;
+        Hamster homa = new Hamster(); // не создали объект для переменной homa
+        System.out.println(homa); // здесь произойдёт сбой
     }
+}
+
+class Hamster {
+    String name = "Хома";
+    int age = 2;
+    double weight = 323.4;
+    boolean isFluffy = true;
+    String color = "Рыжий";
 }
