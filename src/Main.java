df939a4be7dc7f53964b7933245d5633fab41557
ph.zh <p.zhavoronkov@gmail.com>
2024-07-11 14:07:57 +0200

    class main. If / else conditions added. class weatherCalendar - method addRainyDay was initialized

--- src/Main.java
+++ src/Main.java
@@ -15,11 +15,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        WeatherCalendar calendar;
-        calendar.month = "Октябрь";
-        calendar.numberOfDays = 31;
-        calendar.rainyDays = 0;
-        calendar.isRainyMonth = false;
+        WeatherCalendar calendar = new WeatherCalendar("Октябрь", 31);
 
         String[] octoberWeather = new String[] {
                 "Солнечно",
@@ -64,15 +60,15 @@ public class Main {
         }
 
         // Проверить, был ли месяц дождливым. Если да - установить значение isRainyMonth
-        if (...) {
-            ...
+        if (calendar.rainyDays > 15) {
+            calendar.isRainyMonth = true;
         }
 
         System.out.println(calendar.month + " " + calendar.year + " года. "
                 + calendar.rainyDays + " дней шёл дождь.");
 
         // Проверить значение isRainyMonth
-        if (...) {
+        if (calendar.isRainyMonth) {
             System.out.println("Это был дождливый месяц.");
         } else {
             System.out.println("Этот месяц был довольно сухим.");
--- src/WeatherCalendar.java
+++ src/WeatherCalendar.java
@@ -2,11 +2,16 @@ class WeatherCalendar {
     String month;
     int numberOfDays;
     int rainyDays;
-    int year;
+    int year = 2020;
     boolean isRainyMonth;
+
+    WeatherCalendar(String monthOctober, int numberOfDaysInOctober) {
+        month = monthOctober;
+        numberOfDays = numberOfDaysInOctober;
+    }
 
     // Увеличить переменную rainyDays на единицу
     public void addRainyDay() {
-            ...
+        rainyDays = rainyDays + 1;
     }
 }
