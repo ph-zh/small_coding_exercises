56c1c6b94211f6952357e0f4019b3ad52b813c05
ph.zh <p.zhavoronkov@gmail.com>
2024-07-18 09:36:27 +0200

    initialization issue example

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-            long bigNumber = 1_720_776_554;
-            System.out.println(bigNumber);
-        }
+            int justVariable;
+            System.out.println(justVariable);
+    }
 }
