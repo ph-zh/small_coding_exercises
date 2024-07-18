34063777f8b0b8dc8929d3a58ef6f65bb7ee09f7
ph.zh <p.zhavoronkov@gmail.com>
2024-07-18 09:53:48 +0200

    initialization of the fields example

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,19 @@
  */
 public class Main {
     public static void main(String[] args) {
-            int justVariable;
-            System.out.println(justVariable);
+        Hamster homa = new Hamster(); // создали объект
+        System.out.println("Имя хомяка: " + homa.name);
+        System.out.println("Возраст хомяка: " + homa.age);
+        System.out.println("Вес хомяка: " + homa.weight);
+        System.out.println("Пушистый ли хомяк: " + homa.isFluffy);
+        System.out.println("Цвет хомяка: " + homa.color);
+
     }
+}
+class Hamster {
+    String name; // не проинициализировали четыре поля
+    int age;
+    double weight;
+    boolean isFluffy;
+    String color = "Рыжий"; // здесь присвоили значение
 }
