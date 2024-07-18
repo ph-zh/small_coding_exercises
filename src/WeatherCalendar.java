ded24f024ebe4aa752db186880e3c9655288693d
ph.zh <p.zhavoronkov@gmail.com>
2024-07-18 10:42:15 +0200

    weather application. Class weatherCalendar updated with constructor and methid addRainyDays. Class main, updated logic for the true value for the condition

--- src/WeatherCalendar.java
+++ src/WeatherCalendar.java
@@ -2,11 +2,16 @@ class WeatherCalendar {
     String month;
     int numberOfDays;
     int rainyDays;
-    int year;
+    int year = 2020;
     boolean isRainyMonth;
 
+
+    WeatherCalendar(String monthName, int monthNumberOfDays) {
+        month = monthName;
+        numberOfDays = monthNumberOfDays;
+    }
     // Увеличить переменную rainyDays на единицу
     public void addRainyDay() {
-            ...
+        rainyDays = rainyDays + 1;
     }
 }
