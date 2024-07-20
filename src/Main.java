1e98f851da836a3990faf67478ccedf7e903ebae
ph.zh <p.zhavoronkov@gmail.com>
2024-07-20 14:52:02 +0200

    work with operands updated

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@
  */
 public class Main {
     public static void main(String[] args) {
-        b /= 3;
-        System.out.println(result);
-        int result = a - b;
-        a *= 5;
         int b = 9;
         int a = 11;
+        b /= 3;
         a += 1;
+        a *= 5;
+        int result = a - b;
+        System.out.println(result);
     }
 }
