5092ae00b5c2d19cf123279b60ba4c9dd2a54a1a
ph.zh <p.zhavoronkov@gmail.com>
2024-07-20 07:33:03 +0200

    heap memory task example

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,29 @@
 /*
-Дополните код так, чтобы теперь существовало два самостоятельных кота.
-Первый с именем Роджер, второй с именем Барсик. Обратите внимание: новый код можно добавлять только в указанной строке.
-Менять остальные строки не следует.
+
  */
 public class Main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Барсик");
-        Cat cat2 = cat1;
-        // вставьте код сюда
-        cat1 = new Cat("Роджер");
-        System.out.println("Кот 1: " + cat1.name + ", кот 2: " + cat2.name);
+        Cat cat = new Cat("Барсик", 4);
+        // stack: {main: cat = [Cat@683]}
+        soSmthInteresting(cat);
+        System.out.println("Это " + cat.name + ", ему " + cat.age + " лет.");
+    }
+
+    static void soSmthInteresting(Cat cat) {
+        // stack: {soSmthInteresting: [cat = [Cat@683], main: cat = [Cat@683]}
+        cat.age++;
+        cat = new Cat("Роджер", cat.age);
+        // stack: {soSmthInteresting: [cat = [Cat@686], main: cat = [Cat@683]}
+        System.out.println("Теперь кота зовут " + cat.name);
     }
 }
 
 class Cat {
     String name;
+    int age;
 
-    public Cat(String catName) {
+    public Cat(String catName, int catAge) {
         name = catName;
+        age = catAge;
     }
 }
