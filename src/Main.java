db62bb7128bc92c786b4b8c6db445799c07dc1a9
ph.zh <p.zhavoronkov@gmail.com>
2024-07-20 22:25:34 +0200

    pizza for the meeting task to train operators

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
 /*
-Допишите код метода, который проверяет, чётное число или нет.
+По мнению Джеффа Безоса, основателя и главы компании Amazon, идеальная встреча — это встреча, для проведения которой хватит двух пицц.
+Помогите Джеффу вычислить, сколько человек можно позвать на встречу, исходя из количества кусочков пиццы на каждого. Также определите,
+останется ли ещё пицца после того, как все разойдутся.
+Вам нужно дополнить реализацию метода printMaximumPeopleToMeeting(), который в качестве аргументов принимает, на сколько кусков порезана
+одна пицца и сколько кусков пиццы съедает один участник встречи.
  */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int numberToCheck = 11;
-        isEven(numberToCheck);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("На сколько кусков порезана пицца?");
+        int slicesInPizza = scanner.nextInt();
+        System.out.println("Сколько кусков съедает один участник встречи?");
+        int slicesToPerson = scanner.nextInt();
+        printMaximumPeopleToMeeting(slicesToPerson, slicesInPizza);
     }
 
-    public static void isEven(int number) {
-        if (number % 2 == 0) { // в условии должна быть проверка на чётность
-            System.out.println(number + " - чётное");
-        } else {
-            System.out.println(number + " - нечётное");
-        }
+    private static void printMaximumPeopleToMeeting(int slicesToPerson, int slicesInPizza) {
+        int numOfPizzas = 2;
+        int totalSlices = ... // Посчитайте общее число кусков пиццы
+        int maximumPeopleToMeeting = ... // На сколько человек хватит пиццы
+        int leftSlices = ... // Сколько пиццы останется
+        System.out.println("Максимальное число участников идеальной встречи: " + maximumPeopleToMeeting);
+        System.out.println("Останется кусков пиццы: " + leftSlices);
     }
 }
