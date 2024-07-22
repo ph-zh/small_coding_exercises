12f23d7d7614bb05aa9378c9f426399aacd3172d
ph.zh <p.zhavoronkov@gmail.com>
2024-07-22 14:01:03 +0200

    increment & decrement task #3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 /*
-
+Разбейте вычисление переменной а на четыре последовательные операции. Используйте дополнительную переменную c.
  */
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 22;
-        int c = (--b / 2) - a++ * 2;
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println("c = " + c);
+        int a = 13;
+        int b = 3;
+
+        a = ++a - a % b++;
+        System.out.println(a + " и " + b);
     }
 }
