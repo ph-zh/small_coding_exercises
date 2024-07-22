ea39c4fd46734f7f17554f97a13011e530e385c0
ph.zh <p.zhavoronkov@gmail.com>
2024-07-22 13:36:31 +0200

    increment & decrement task

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 /*
-Чем больше уроков и учебных задач, тем больше выпитых чашек кофе и переменных в коде.
- На этой неделе ваш одногруппник Игорь прошёл 8 уроков, решил 6 задач и выпил 3 чашки кофе.
- Определите, сколько переменных он объявил в коде — решите пример, используя математический порядок действий.
+
  */
 public class Main {
     public static void main(String[] args) {
-        int classes = 8;
-        int tasks = 6;
-        int coffeeCups = 3;
-        int variables = coffeeCups - classes * tasks + tasks / coffeeCups + (classes + coffeeCups) * tasks;
-        System.out.println(variables);
+        int a = 10;
+        int b = 22;
+        int c = (--b / 2) - a++ * 2;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
     }
 }
