85a508de71778bf8f7c020a0f5ef91dc0d9433f4
ph.zh <p.zhavoronkov@gmail.com>
2024-07-22 15:31:52 +0200

    sctooter renting application. Updated class scooter with wrong logic of the rent price

--- src/Scooter.java
+++ src/Scooter.java
@@ -1,3 +1,16 @@
+/*
+4/ Метод getPrice() должен вычислять текущую стоимость аренды currentPrice. Для этого к числу арендованных скутеров
+scootersInUse надо прибавить 1, результат разделить на число доступных скутеров availableScooters.
+Полученное частное умножить на добавочную стоимость additionalPrice и прибавить к цене по умолчанию defaultPrice.
+5/ Метод rentScooter() должен обрабатывать выдачу самоката и сообщать администратору текущую цену его аренды.
+Чтобы это сделать, нужно модифицировать формулу currentPrice — перед проведением расчётов увеличить на единицу значение
+переменной scootersInUse, а после их окончания уменьшить на единицу значение переменной availableScooters.
+6/ Вычисления в методах getPrice() и rentScooter() необходимо провести одной строкой. Перед выполнением арифметических
+выражений в обоих методах нужно проверить наличие доступных самокатов.
+7/ Метод returnScooter() должен учитывать возврат самоката: уменьшать на единицу число арендованных самокатов и увеличивать
+на единицу число доступных самокатов. При приёме нужно не забыть проверить, есть ли вообще несданные самокаты.
+ */
+
 class Scooter {
     int availableScooters;
     int scootersInUse;
@@ -10,19 +23,19 @@ class Scooter {
     }
 
     void getPrice() {
-        ... { // Проверьте, есть ли доступные самокаты
+        if (availableScooters <= 0) { // Проверьте, есть ли доступные самокаты
             System.out.println("Нет доступных самокатов.");
         } else {
-            int currentPrice = ... // Посчитайте текущую стоимость проката
+            int currentPrice = (scootersInUse / availableScooters * additionalPrice + defaultPrice) + 1; // Посчитайте текущую стоимость проката
             System.out.println("Текущая стоимость проката: " + currentPrice + " руб/мин");
         }
     }
 
     void rentScooter() {
-        if (...) { // Проверьте, есть ли доступные самокаты
+        if (availableScooters <= 0) { // Проверьте, есть ли доступные самокаты
             System.out.println("Доступных самокатов не осталось.");
         } else {
-            int currentPrice = ... /* Посчитайте текущую стоимость проката,
+            int currentPrice = (scootersInUse / availableScooters * additionalPrice + defaultPrice) + 1; /* Посчитайте текущую стоимость проката,
             увеличьте число арендованных самокатов и уменьшите число доступных */
             System.out.println("Выдайте самокат по цене " + currentPrice + " руб/мин");
             System.out.println("Самокатов в аренде: " + scootersInUse);
@@ -31,10 +44,12 @@ class Scooter {
     }
 
     void returnScooter() {
-        ... { // Проверьте, есть ли самокаты в аренде
+        if(availableScooters >= 1) { // Проверьте, есть ли самокаты в аренде
             System.out.println("Все самокаты уже возвращены.");
         } else {
-            ... // Уменьшите число арендованных самокатов и увеличьте число доступных
+            // Уменьшите число арендованных самокатов и увеличьте число доступных
+            --scootersInUse;
+            ++availableScooters;
             System.out.println("Самокат принят.");
             System.out.println("Самокатов в аренде: " + scootersInUse);
             System.out.println("Самокатов доступно: " + availableScooters);
