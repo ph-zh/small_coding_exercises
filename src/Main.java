d0975052ace16b57f4e1fa387ac598ab9785350c
ph.zh <p.zhavoronkov@gmail.com>
2024-07-23 22:16:39 +0200

    flight ticket application logic updated

--- src/Main.java
+++ src/Main.java
@@ -20,18 +20,17 @@ public class Main {
             System.out.println("Когда планируется путешествие? Введите номер месяца от 1 до 12.");
             monthNumber = scanner.nextInt();
 
-            if (monthNumber > 12) { // Инвертируйте условие
+            if (!(monthNumber > 12)) { // Инвертируйте условие
                 break;
             } else {
                 System.out.println("Некорректный номер месяца. Введите ещё раз.");
             }
         }
         String season = getSeasonByNumber(monthNumber);
-
-        ... /* Допишите ветвление, которое будет печатать строку:
+        if (season.equals("Лето")) {
             System.out.println("Летом лучше остаться в Москве.");
-              и завершать выполнение программы */
-
+            return;
+        }
 
         System.out.println("Укажите стоимость прямых билетов из Москвы в Париж:");
         int ticketMoscowParis = scanner.nextInt();
@@ -42,11 +41,8 @@ public class Main {
         System.out.println("0 - визы нет");
         int britainVisa = scanner.nextInt();
 
-
-        // В этом ветвлении что-то напутано с условиями - исправьте это
-        if (!season.equals("Лето")) { // Перенесите проверку на "Лето" в ветвление выше
-            if (!(ticketMoscowLondonParis <= ticketMoscowParis)) {
-                if (!(britainVisa == 0)) { // Упростите это условие
+            if (ticketMoscowLondonParis <= ticketMoscowParis) {
+                if (britainVisa == 1) { // Упростите это условие
                     System.out.println("Летим через Лондон!");
                 } else {
                     System.out.println("Летим напрямую в Париж!");
@@ -54,18 +50,16 @@ public class Main {
             } else {
                 System.out.println("Летим напрямую в Париж!");
             }
-        }
     }
 
     private static String getSeasonByNumber(int monthNumber) {
-        // Допишите условия ветвления в виде утверждений
-        if (...) {
+        if (monthNumber <= 2) {
             return "Зима";
-        } else if (...) {
+        } else if (monthNumber <= 5) {
             return "Весна";
-        } else if (...) {
+        } else if (monthNumber <= 8) {
             return "Лето";
-        } else if (...) {
+        } else if (monthNumber <= 11) {
             return "Осень";
         } else {
             return "Зима";
