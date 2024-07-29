9e2e43808074d1baddbde62bd41133aa19b96cb3
ph.zh <p.zhavoronkov@gmail.com>
2024-07-29 13:34:29 +0200

    ArrayList value adding

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-    ArrayList<String> studentNames = new ArrayList<>();
+        ArrayList<Double> expenses = new ArrayList<>();
+        expenses.add(777.2);
+        expenses.add(120.43);
+        expenses.add(290.34);
+        expenses.add(420.1);
+        System.out.println(expenses);
     }
 }
