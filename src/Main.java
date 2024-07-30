d543fc90fccb3521783126f1badb36df73c9b9ff
ph.zh <p.zhavoronkov@gmail.com>
2024-07-30 14:17:53 +0200

    short cycle task template

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 /*
-
+В списке speeds хранится информация о скоростях автомобиля на разных участках дороги.
+С помощью короткой формы цикла for и метода size() посчитайте среднюю скорость.
  */
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Double> expenses = new ArrayList<>();
-        expenses.add(120.23);
-        expenses.add(290.32);
-        expenses.add(420.03);
+        ... speeds = ...; // Объявите список
+        speeds.add(120);
+        speeds.add(75);
+        speeds.add(42);
+        speeds.add(60);
+        speeds.add(110);
+        speeds.add(20);
 
-        System.out.println("Сейчас в списке: ");
-        for (Double exp : expenses) {
-            System.out.println("Трата на сумму " + exp + " руб.");
+        int sum = 0;
+        for (... speed ...) {
+            ...;  // Сложите все значения в списке
         }
+
+        int averageSpeed = ... // Вычислите среднюю скорость
+        System.out.println("Средняя скорость равна " + ... + " км/ч");
     }
 }
