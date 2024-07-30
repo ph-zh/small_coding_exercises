933ef1378bc958596a343967f613087700f9586c
ph.zh <p.zhavoronkov@gmail.com>
2024-07-30 11:41:34 +0200

    Array replacement to ArrayList task updated

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        double[] temperatures = new double[4];
-        temperatures[0] = 92.3;
-        temperatures[1] = 12.4;
-        temperatures[2] = 74.1;
-        temperatures[3] = 45.0;
+        ArrayList<Double> temperatures = new ArrayList<>();
+        temperatures.add(92.3);
+        temperatures.add(12.4);
+        temperatures.add(74.1);
+        temperatures.add(45.0);
 
         double max = 0;
-        for (int i = 0; i < temperatures.length; i++) {
-            if (temperatures[i] > max) {
-                max = temperatures[i];
+        for (int i = 0; i < temperatures.size(); i++) {
+            if (temperatures.get(i) > max) {
+                max = temperatures.get(i);
             }
         }
         System.out.println("Самая высокая температура: " + max + " °С.");
