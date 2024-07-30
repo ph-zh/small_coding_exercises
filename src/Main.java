a9d1be164ed3a41f003fd8c675d8c46c1e133bb3
ph.zh <p.zhavoronkov@gmail.com>
2024-07-30 10:47:43 +0200

    ArrayList String task

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Double> expenses = new ArrayList<>();
-        expenses.add(777.2);
-        expenses.add(120.43);
-        expenses.add(290.34);
-        expenses.add(420.1);
-        System.out.println(expenses);
+        ... femaleNames = ...; // Создайте список
+        ...("Екатерина");
+        ...("Мария");
+        ...("Анна");
+        ...(...); // Добавьте имя "Наталья"
+        ... // Распечайте список
     }
 }
