676b61908e9b9199467d47173abbba4e3c8aceba
ph.zh <p.zhavoronkov@gmail.com>
2024-07-30 15:06:07 +0200

    ArrayList. Zoo cycle list updated

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,29 @@
 учитывайте, что первым кормят крокодила, затем слона, ещё через час — шиншиллу, в полдень — льва, а последним — медведя.
 Для печати животных используйте сокращённый цикл for. Весь необходимый текст вы найдёте в комментариях — от вас нужен только код.
  */
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Шиншилла");
+        animals.add("Крокодил");
+        animals.add("Лев");
+        animals.add("Медведь");
+        animals.add("Слон");
         // "Сегодня в зоопарке можно увидеть кормления " + ... + " животных:"
         // "Это будут:"
-        // "Шиншилла"
-        // "Крокодил"
-        // "Лев"
-        // "Медведь"
-        // "Слон"
-        // "Расписание кормлений:"
-        // "В 9:00 - "
-        // "В 10:00 - "
-        // "В 11:00 - "
-        // "В 12:00 - "
-        // "В 13:00 - "
+        System.out.println("Сегодня в зоопарке можно увидеть кормления " + animals.size() + " животных:");
+        System.out.println("Это будут:");
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
+
+        System.out.println("Расписание кормлений:");
+        System.out.println("В 9:00 - " + animals.get(1));
+        System.out.println("В 10:00 - " + animals.get(4));
+        System.out.println("В 11:00 - " + animals.get(0));
+        System.out.println("В 12:00 - " + animals.get(2));
+        System.out.println("В 13:00 - " + animals.get(3));
     }
 }
