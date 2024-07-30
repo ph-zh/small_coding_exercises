ab64201c0566d17bbc2646abc12771b0f51502a1
ph.zh <p.zhavoronkov@gmail.com>
2024-07-30 10:52:40 +0200

    ArrayList String task, ArrayList updated

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ... femaleNames = ...; // Создайте список
-        ...("Екатерина");
-        ...("Мария");
-        ...("Анна");
-        ...(...); // Добавьте имя "Наталья"
-        ... // Распечайте список
+        ArrayList<String> femaleNames = new ArrayList<>(); // Создайте список
+        femaleNames.add("Екатерина");
+        femaleNames.add("Мария");
+        femaleNames.add("Анна");
+        femaleNames.add(1,"Наталья"); // Добавьте имя "Наталья"
+        System.out.println(femaleNames); // Распечайте список
     }
 }
